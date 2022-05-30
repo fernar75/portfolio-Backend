9a6049bade53e03b2365d30b345399a1954d18a3
fernar75 <94242277+fernar75@users.noreply.github.com>
2022-05-30 12:07:15 -0300

    Cors

--- src/main/java/ar/com/portfolio/portfolio/PortfolioApplication.java
+++ src/main/java/ar/com/portfolio/portfolio/PortfolioApplication.java
@@ -22,16 +22,16 @@ public class PortfolioApplication {
 
 
 	//********Cors*********//
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
-//			}
-//		};
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 
-//	}
+	}
         
 
 //*****************************************************************************************************//        
